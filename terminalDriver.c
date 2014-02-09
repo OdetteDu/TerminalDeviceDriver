36dfd90e4fef64951401a963078a1038f2bcac8f
OdetteDu <Odette.Du@rice.edu>
2014-02-09 15:13:13 -0600

    created terminalDriver

---
+++ terminalDriver.c
@@ -0,0 +1,39 @@
+#include <hardware.h>
+#include <terminals.h>
+
+void ReceiveInterrupt(int term)
+{
+	  
+}
+
+void TransmitInterrupt(int term)
+{
+	  
+}
+
+int WriteTerminal(int term, char *buf, int buflen)
+{
+	  return 0;
+}
+
+int ReadTerminal(int term, char *buf, int buflen)
+{
+	  return 0;
+}
+
+int InitTerminal(int term)
+{
+	  return 0;
+}
+
+int TerminalDriverStatistics(struct termstat *stats)
+{
+	  return 0;
+}
+
+int InitTerminalDriver()
+{
+	  return 0;
+}
+
+
